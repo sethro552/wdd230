17a4fe1dc300db3d20544dba1ba9c392c30469b7
sethro552 <mar16046@byui.edu>
2022-11-15 20:26:33 -0500

    lesson

--- lesson4/chamber/scripts/script.js
+++ lesson4/chamber/scripts/script.js
@@ -29,4 +29,11 @@ function scrollDown(elem) {
     else {
         document.querySelector('#scroll-down').className = ""
     }
+    const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
 }
