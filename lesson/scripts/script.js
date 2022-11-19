3d268db0c72cdb09b0c90313dc744d7a57a70387
sethro552 <mar16046@byui.edu>
2022-11-19 16:00:13 -0500

    lesson

--- lesson/scripts/script.js
+++ lesson/scripts/script.js
@@ -1,32 +1,6 @@
-WebFont.load({
-    google: {
-        families: [
-            'Gugi'
-        ]
-    }
-})
+let d = new Date();
+let fulldate = d.getFullYear();
+document.getElementById("currentdate").textContent = fulldate;
 
-
-// Set the year for copyright
-document.getElementById("year").innerHTML = new Date().getFullYear()
-
-// Set the date when last updated
-var temp = document.lastModified
-var n = parseInt(temp.substring(temp.length-8,temp.length-6))
-if (n>12) {
-    document.getElementById("updated").innerHTML = "Last Updated: "+
-        temp.substring(0,temp.length-8)+(n-12)+temp.substring(temp.length-6)+" pm"
-}
-else {
-    document.getElementById("updated").innerHTML = "Last Updated: "+
-        temp+" am"
-}
-
-function scrollDown(elem) {
-    if (elem.scrollTop < 100) {
-        document.querySelector('#scroll-down').className = "up"
-    }
-    else {
-        document.querySelector('#scroll-down').className = ""
-    }
-}
+const Update = document.lastModified
+document.querySelector("#update").textContent = Update;
