2e545254b3273950a8c62350b9d17700047198a7
sethro552 <mar16046@byui.edu>
2023-03-21 01:58:37 -0400

    portal

--- lesson10/scripts/weatherapi.js
+++ lesson10/scripts/weatherapi.js
@@ -1,34 +1,39 @@
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=64.8378&lon=-147.7164&units=imperial&appid=712d41b63fbf8971f61376c9e8ac70a6'
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&appid=0fd4b8507deab2af8643f8ce98cb4ab2&units=imperial";
 
 async function apiFetch() {
-    try {
-      const response = await fetch(url);
-      if (response.ok) {
-        const data = await response.json();
-        console.log(data);
-        displayResults(data);
-      } else {
-          throw Error(await response.text());
-      }
-    } catch (error) {
-        console.log(error);
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // this is for testing the call
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
     }
+  } catch (error) {
+    console.log(error);
   }
-  
-  apiFetch();
+}
+
+apiFetch();
 
 function displayResults(weatherData) {
-    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(
+    0
+  )}</strong>`;
 
-    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
-    let desc = weatherData.weather[0].description;
-    let descUp = desc.toUpperCase();
-  
-    weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', descUp);
-    captionDesc.textContent = descUp;
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+
+  weatherIcon.setAttribute("src", iconsrc);
+  weatherIcon.setAttribute("alt", desc);
+  captionDesc.textContent = desc;
 }
