71d9e16d7e9c515ebab5626dca93b05c46897282
sethro552 <mar16046@byui.edu>
2023-03-22 22:27:10 -0400

    portal

--- lesson7/scripts/lazyscripts.js
+++ lesson7/scripts/lazyscripts.js
@@ -6,28 +6,4 @@ const lazyLoad = (img) => {
         img.removeAttribute("data-src")
         img.className = "in"
     };
-};
-
-const options = {
-    threshold: 0,
-    rootMargin: "0px 0px 50px 0px"
-}
-
-if ('IntersectionObserver' in window) {
-    const obsrvr = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
-            if(item.isIntersecting) {
-                lazyLoad(item.target)
-                observer.unobserve(item.target)
-            }
-        })
-    }, options)
-    allImages.forEach((img) => {
-        obsrvr.observe(img)
-    })
-}
-else {
-    allImages.forEach((img) => {
-        lazyLoad(img)
-    })
 }
