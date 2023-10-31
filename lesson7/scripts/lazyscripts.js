372e11723ee117ecb49c16243258a9186348dadd
sethro552 <mar16046@byui.edu>
2023-10-31 03:24:24 -0400

    portal

--- lesson7/scripts/lazyscripts.js
+++
@@ -1,9 +0,0 @@
-const allImages = document.querySelectorAll("img[data-src]")
-
-const lazyLoad = (img) => {
-    img.setAttribute("src", img.getAttribute("data-src"))
-    img.onload = () => {
-        img.removeAttribute("data-src")
-        img.className = "in"
-    };
-}
