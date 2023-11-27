d0877831e8645a8596083c99d7ea1b0782c2f2c8
sethro552 <mar16046@byui.edu>
2023-11-26 23:55:53 -0500

    portal

--- scripts/links.js
+++ scripts/links.js
@@ -4,7 +4,5 @@ const linksURL = "https://sethro552.github.io/wdd230/data/links.json";
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
-  }
-  
-  getLinks();
+    displayLinks(weeks);
+}
